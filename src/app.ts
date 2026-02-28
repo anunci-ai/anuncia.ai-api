1fa7f07eca7bcb5602a79f291fca1114329c5c47
Alvaro Sena <alvarosenacs.c@gmail.com>
2026-02-28 13:07:45 -0300

    fix: pre linter git commit message

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,10 @@
 import express from "express";
-import { prisma } from "./prisma";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", async (request, response) => {
-  const name = "Dimitri";
-  const email = "dimitri@example.com";
+const port = process.env.PORT || 8080;
 
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-    },
-  });
-
-  return response.json({ user });
-});
-
-app.get("/:email", async (request, response) => {
-  const { email } = request.params;
-
-  const user = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  return response.json({ user });
-});
-
-app.listen(8080, () => {
-  console.log("Server is running");
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
